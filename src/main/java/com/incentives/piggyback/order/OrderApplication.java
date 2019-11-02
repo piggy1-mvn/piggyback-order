39df6267219c07513aafc8200429fc8f9affe041
Rajan Gupta <rajan0493@gmail.com>
2019-11-02 18:51:39 +0800

    Jasypt encryption

--- src/main/java/com/incentives/piggyback/order/OrderApplication.java
+++ src/main/java/com/incentives/piggyback/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package com.incentives.piggyback.order;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class OrderApplication {
 
 	public static void main(String[] args) {
